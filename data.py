9f857612b9f7aa92ab5b8bddddc34f0dd0a9a283
MOHAMMED SHOEBUDDIN HABEEB <f2014895@hyderabad.bits-pilani.ac.in>
2020-04-04 18:29:29 -0400

    Add files via upload

---
+++ data.py
@@ -0,0 +1,112 @@
+from os.path import exists, join, basename
+from os import makedirs, remove
+from six.moves import urllib
+import tarfile
+from torchvision.transforms import Compose, CenterCrop, ToTensor, Resize
+
+from dataset import DatasetFromFolder
+
+class Test(ToTensor):
+    def __call__(self, pic):
+        """
+        Args:
+            pic (PIL.Image or numpy.ndarray): Image to be converted to tensor.
+
+        Returns:
+            Tensor: Converted image.
+        """
+        if isinstance(pic, np.ndarray):
+            # handle numpy array
+            img = torch.from_numpy(pic.transpose((2, 0, 1)))
+            # backward compatibility
+            return img.float().div(3700)
+
+        if accimage is not None and isinstance(pic, accimage.Image):
+            nppic = np.zeros([pic.channels, pic.height, pic.width], dtype=np.float32)
+            pic.copyto(nppic)
+            return torch.from_numpy(nppic)
+
+        # handle PIL Image
+        if pic.mode == 'I':
+            img = torch.from_numpy(np.array(pic, np.int32, copy=False))
+        elif pic.mode == 'I;16':
+            img = torch.from_numpy(np.array(pic, np.int16, copy=False))
+        else:
+            img = torch.ByteTensor(torch.ByteStorage.from_buffer(pic.tobytes()))
+        # PIL image mode: 1, L, P, I, F, RGB, YCbCr, RGBA, CMYK
+        if pic.mode == 'YCbCr':
+            nchannel = 3
+        elif pic.mode == 'I;16':
+            nchannel = 1
+        else:
+            nchannel = len(pic.mode)
+        img = img.view(pic.size[1], pic.size[0], nchannel)
+        # put it from HWC to CHW format
+        # yikes, this transpose takes 80% of the loading time/CPU
+        img = img.transpose(0, 1).transpose(0, 2).contiguous()
+        if isinstance(img, torch.ByteTensor):
+            return img.float().div(3700)
+        else:
+            return img
+
+def download_bsd300(dest="Dataset_M"):
+    output_image_dir = join(dest, "images")
+
+    if not exists(output_image_dir):
+        makedirs(dest)
+        url = "http://www2.eecs.berkeley.edu/Research/Projects/CS/vision/bsds/BSDS300-images.tgz"
+        print("error")
+
+        data = urllib.request.urlopen(url)
+
+        file_path = join(dest, basename(url))
+        with open(file_path, 'wb') as f:
+            f.write(data.read())
+
+        print("Extracting data")
+        with tarfile.open(file_path) as tar:
+            for item in tar:
+                tar.extract(item, dest)
+
+        remove(file_path)
+
+    return output_image_dir
+
+
+def calculate_valid_crop_size(crop_size, upscale_factor):
+    return crop_size - (crop_size % upscale_factor)
+
+
+def input_transform(crop_size, upscale_factor):
+    return Compose([
+        CenterCrop(crop_size),
+        Resize(crop_size // upscale_factor),
+        Test(),
+    ])
+
+
+def target_transform(crop_size):
+    return Compose([
+        CenterCrop(crop_size),
+        Test(),
+    ])
+
+
+def get_training_set(upscale_factor):
+    root_dir = download_bsd300()
+    train_dir = join(root_dir, "train")
+    crop_size = calculate_valid_crop_size(256, upscale_factor)
+
+    return DatasetFromFolder(train_dir,
+                             input_transform=input_transform(crop_size, upscale_factor),
+                             target_transform=target_transform(crop_size))
+
+
+def get_test_set(upscale_factor):
+    root_dir = download_bsd300()
+    test_dir = join(root_dir, "test")
+    crop_size = calculate_valid_crop_size(256, upscale_factor)
+
+    return DatasetFromFolder(test_dir,
+                             input_transform=input_transform(crop_size, upscale_factor),
+                             target_transform=target_transform(crop_size))
