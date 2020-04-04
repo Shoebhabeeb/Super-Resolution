90a5bab1026c860b6c6fae0a38262328cda1da8f
MOHAMMED SHOEBUDDIN HABEEB <f2014895@hyderabad.bits-pilani.ac.in>
2020-04-04 19:50:08 -0400

    Update super_resolve.py

--- super_resolve.py
+++ super_resolve.py
@@ -6,6 +6,48 @@ from torchvision.transforms import ToTensor
 
 import numpy as np
 
+class Test(ToTensor):
+    def __call__(self, pic):
+        """
+        Args:
+            pic (PIL.Image or numpy.ndarray): Image to be converted to tensor.
+        Returns:
+            Tensor: Converted image.
+        """
+        if isinstance(pic, np.ndarray):
+            # handle numpy array
+            img = torch.from_numpy(pic.transpose((2, 0, 1)))
+            # backward compatibility
+            return img.float().div(3700)
+
+        if accimage is not None and isinstance(pic, accimage.Image):
+            nppic = np.zeros([pic.channels, pic.height, pic.width], dtype=np.float32)
+            pic.copyto(nppic)
+            return torch.from_numpy(nppic)
+
+        # handle PIL Image
+        if pic.mode == 'I':
+            img = torch.from_numpy(np.array(pic, np.int32, copy=False))
+        elif pic.mode == 'I;16':
+            img = torch.from_numpy(np.array(pic, np.int16, copy=False))
+        else:
+            img = torch.ByteTensor(torch.ByteStorage.from_buffer(pic.tobytes()))
+        # PIL image mode: 1, L, P, I, F, RGB, YCbCr, RGBA, CMYK
+        if pic.mode == 'YCbCr':
+            nchannel = 3
+        elif pic.mode == 'I;16':
+            nchannel = 1
+        else:
+            nchannel = len(pic.mode)
+        img = img.view(pic.size[1], pic.size[0], nchannel)
+        # put it from HWC to CHW format
+        # yikes, this transpose takes 80% of the loading time/CPU
+        img = img.transpose(0, 1).transpose(0, 2).contiguous()
+        if isinstance(img, torch.ByteTensor):
+            return img.float().div(3700)
+        else:
+            return img
+        
 # Training settings
 parser = argparse.ArgumentParser(description='PyTorch Super Res Example')
 parser.add_argument('--input_image', type=str, required=True, help='input image to use')
@@ -19,7 +61,7 @@ img = Image.open(opt.input_image).convert('YCbCr')
 y, cb, cr = img.split()
 
 model = torch.load(opt.model)
-img_to_tensor = ToTensor()
+img_to_tensor = Test()
 input = img_to_tensor(y).view(1, -1, y.size[1], y.size[0])
 
 if opt.cuda:
@@ -29,8 +71,8 @@ if opt.cuda:
 out = model(input)
 out = out.cpu()
 out_img_y = out[0].detach().numpy()
-out_img_y *= 255.0
-out_img_y = out_img_y.clip(0, 255)
+out_img_y *= 3700
+out_img_y = out_img_y.clip(0, 3700)
 out_img_y = Image.fromarray(np.uint8(out_img_y[0]), mode='L')
 
 out_img_cb = cb.resize(out_img_y.size, Image.BICUBIC)
